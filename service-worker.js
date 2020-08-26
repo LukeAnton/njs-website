a1476dc5540151c0fa720b8955f415a001f95c7f
luke firth <hilukeyouarelookingsexytoday@192-168-1-117.tpgi.com.au>
2020-08-26 11:18:16 +1000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/njsprojects.com/precache-manifest.11e10c3d04e340f9d55d0b8e4d2f2e55.js"
+  "/precache-manifest.c960ebd39f09d6424e9cdcb6c293f0a9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/njsprojects.com/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
